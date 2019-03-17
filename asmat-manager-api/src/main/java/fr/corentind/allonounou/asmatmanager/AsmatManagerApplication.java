8fa0290b1a16e28cd14061565cb998baf7fc146d
Corentin Delannoy <corentin.delannoy2@gmail.com>
2019-03-17 11:42:40 +0100

    First commit

---
+++ asmat-manager-api/src/main/java/fr/corentind/allonounou/asmatmanager/AsmatManagerApplication.java
@@ -0,0 +1,13 @@
+package fr.corentind.allonounou.asmatmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmatManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsmatManagerApplication.class, args);
+    }
+
+}
