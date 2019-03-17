7ff8474b5d4628dd0d9209e6fd5d38abf50e8d6a
Corentin Delannoy <corentin.delannoy2@gmail.com>
2019-03-17 18:04:21 +0100

    feat(list): now displays asmat count

--- asmat-manager-front/src/app/home/list-asmats/list-asmats.component.ts
+++ asmat-manager-front/src/app/home/list-asmats/list-asmats.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit} from '@angular/core';
 import {Asmat} from '../../model/asmat';
 import {AsmatService} from '../../service/asmat.service';
 import {AsmatFilter} from './asmat-filter';
+import {AsmatFilterPipe} from './asmat-filter.pipe';
 
 @Component({
   selector: 'app-list-asmats',
@@ -10,14 +11,17 @@ import {AsmatFilter} from './asmat-filter';
 })
 export class ListAsmatsComponent implements OnInit {
 
-  public asmats: Asmat[];
   public searchInput: string;
   public adherentOnly: boolean;
+
+  private asmats: Asmat[];
+  private asmatFilterPipe: AsmatFilterPipe;
 
   constructor(private asmatService: AsmatService) {
     this.asmats = [];
     this.searchInput = '';
     this.adherentOnly = false;
+    this.asmatFilterPipe = new AsmatFilterPipe();
   }
 
   public ngOnInit() {
@@ -25,12 +29,14 @@ export class ListAsmatsComponent implements OnInit {
       .subscribe(asmats => this.asmats = asmats);
   }
 
-  public get filter(): AsmatFilter {
+  public get filteredAsmats(): Asmat[] {
+    return this.asmatFilterPipe.transform(this.asmats, this.filter);
+  }
+
+  private get filter(): AsmatFilter {
     return {
       keywords: this.searchInput,
       adherent: this.adherentOnly
     };
   }
-
-
 }
