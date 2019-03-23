27e4fe4bfaee5bfb844d0c2bad39cfa15b058aad
Corentin Delannoy <corentin.delannoy2@gmail.com>
2019-03-23 18:01:27 +0100

    feat(validation): now handles validation in app

--- asmat-manager-front/src/app/home/add-asmat/add-asmat.component.ts
+++ asmat-manager-front/src/app/home/add-asmat/add-asmat.component.ts
@@ -2,6 +2,10 @@ import {Component} from '@angular/core';
 import {Asmat} from '../../model/asmat';
 import {AsmatService} from '../../service/asmat.service';
 import {Router} from '@angular/router';
+import {catchError} from 'rxjs/operators';
+import {HttpErrorResponse} from '@angular/common/http';
+import {ToastService} from '../../service/toast.service';
+import {NEVER, throwError} from 'rxjs';
 
 @Component({
   selector: 'app-add-asmat',
@@ -11,10 +15,20 @@ import {Router} from '@angular/router';
 export class AddAsmatComponent {
 
   constructor(private asmatService: AsmatService,
-              private router: Router) { }
+              private router: Router,
+              private toaster: ToastService) {
+  }
 
   public onAsmatSubmitted(asmat: Asmat) {
     this.asmatService.create(asmat)
+      .pipe(catchError((err: HttpErrorResponse) => {
+        if (err.status === 400) {
+          this.toaster.open(err.error.message, 5000);
+          return NEVER;
+        } else {
+          return throwError(err);
+        }
+      }))
       .subscribe(() => this.router.navigate(['home']));
   }
 }
--- asmat-manager-front/src/app/home/update-asmat/update-asmat.component.ts
+++ asmat-manager-front/src/app/home/update-asmat/update-asmat.component.ts
@@ -2,6 +2,10 @@ import {Component, OnInit} from '@angular/core';
 import {AsmatService} from '../../service/asmat.service';
 import {Asmat} from '../../model/asmat';
 import {ActivatedRoute, Router} from '@angular/router';
+import {catchError} from 'rxjs/operators';
+import {HttpErrorResponse} from '@angular/common/http';
+import {NEVER, throwError} from 'rxjs';
+import {ToastService} from '../../service/toast.service';
 
 @Component({
   selector: 'app-update-asmat',
@@ -14,7 +18,8 @@ export class UpdateAsmatComponent implements OnInit {
 
   constructor(private asmatService: AsmatService,
               private route: ActivatedRoute,
-              private router: Router) {
+              private router: Router,
+              private toaster: ToastService) {
     this.asmat = null;
   }
 
@@ -25,7 +30,14 @@ export class UpdateAsmatComponent implements OnInit {
   }
 
   public onAsmatSubmitted(asmat: Asmat) {
-    this.asmatService.update(asmat)
+    this.asmatService.update(asmat).pipe(catchError((err: HttpErrorResponse) => {
+      if (err.status === 400) {
+        this.toaster.open(err.error.message, 5000);
+        return NEVER;
+      } else {
+        return throwError(err);
+      }
+    }))
       .subscribe(() => this.router.navigate(['home']));
   }
 }
