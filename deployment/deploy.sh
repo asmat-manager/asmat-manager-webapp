bf5ed342d886e5c4412f3007eef51a1672b2d28d
Corentin Delannoy <corentin.delannoy2@gmail.com>
2019-09-14 18:24:11 +0200

    chore(travis): fixed insecure docker registry

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -2,14 +2,16 @@ set -ev
 
 DOCKER_TAG=$1
 DOCKER_IMAGE="manager.allonounouicibebe.fr:5000/manager-webapp:$DOCKER_TAG"
-
 SERVICE_PATH="/home/corentin/services/$2"
 
 docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
 docker build -t $DOCKER_IMAGE .
 docker push $DOCKER_IMAGE
 
-ssh -p 2220 manager.allonounouicibebe.fr << EOF
+eval "$(ssh-agent -s)"
+ssh-add deployment/id_rsa
+
+ssh -p 2220 -o BatchMode=yes -o StrictHostKeyChecking=no corentin@manager.allonounouicibebe.fr << EOF
 cd $SERVICE_PATH
 docker-compose down
 docker-compose pull
